5fffb4c8d5fa159ebd784a7a37f2caa5742f2883
aaronrhim <rhimaaron@gmail.com>
2025-11-03 16:08:56 -0800

    updated app settings to fix project title positioning

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MarsParallaxBackground } from "@/components/MarsParallaxBackground";
 import Index from "./pages/Index";
+import ProjectDetail from "./pages/ProjectDetail";
+import AllProjects from "./pages/AllProjects";
+import AboutMe from "./pages/AboutMe";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -14,8 +18,12 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <MarsParallaxBackground />
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/projects" element={<AllProjects />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/project/:projectId" element={<ProjectDetail />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
